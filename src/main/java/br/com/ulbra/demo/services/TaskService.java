8b1419179869d7089014a25044dedca175a4b768
Rodrigo Sandri <102749822+RodrigoSandri@users.noreply.github.com>
2026-02-25 18:51:52 -0300

    Create TaskService

---
+++ src/main/java/br/com/ulbra/demo/services/TaskService.java
@@ -0,0 +1,51 @@
+package com.guigosandri.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ulbra.demo.models.Task;
+import br.com.ulbra.demo.models.User;
+import br.com.ulbra.demo.repositories.TaskRepository;
+import br.com.ulbra.demo.models;
+
+@Service
+public class TaskService {
+
+    @Autowired  
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada! Id: " + id));
+    }
+    
+    @Transactional
+    public Task create (Task obj) {
+        User user = this.userService.findById(obj.getUser.getId);
+        obj.setId(null);
+        obj.setUser(user);
+        obj.this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId);
+        newObj.setDescription(obj.getDescription);
+        return this.taskRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {   
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir, pois há entidades relacionada!");
+        }
+    }
+}
