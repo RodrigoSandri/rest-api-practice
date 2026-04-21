17fd5688b6c3f5c77b575c8065939fa714f82088
Rodrigo Sandri <102749822+RodrigoSandri@users.noreply.github.com>
2026-04-20 21:26:06 -0300

    Refatora UserService para criptografar senhas com BCrypt

--- src/main/java/br/com/ulbra/demo/services/UserService.java
+++ src/main/java/br/com/ulbra/demo/services/UserService.java
@@ -2,43 +2,41 @@ package br.com.ulbra.demo.services;
 
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.com.ulbra.demo.repositories.UserRepository;
 import br.com.ulbra.demo.models.User;
-
+import br.com.ulbra.demo.repositories.UserRepository;
 
 @Service
 public class UserService {
-    
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
 
-
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
-    return user.orElseThrow(() -> new RuntimeException(
-            "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
-        ));
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
     }
 
     @Transactional
     public User create(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         obj = this.userRepository.save(obj);
         return obj;
-
     }
 
     @Transactional
     public User update(User obj) {
         User newObj = findById(obj.getId());
-        newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
@@ -50,8 +48,4 @@ public class UserService {
             throw new RuntimeException("Não é possível excluir, pois há entidades relacionadas!");
         }
     }
-
-
 }
-
-   
