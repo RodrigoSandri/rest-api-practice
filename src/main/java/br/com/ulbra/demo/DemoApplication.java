847a5fb795f79d28581aa9017f4913dd3a2f7644
Rodrigo Sandri <102749822+RodrigoSandri@users.noreply.github.com>
2026-04-20 22:43:58 -0300

    Configura PasswordConfig criando bean BCryptPasswordEncoder para suporte à criptografia de senhas no Spring Security

--- src/main/java/br/com/ulbra/demo/DemoApplication.java
+++ src/main/java/br/com/ulbra/demo/DemoApplication.java
@@ -3,11 +3,10 @@ package br.com.ulbra.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.ulbra.demo")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
